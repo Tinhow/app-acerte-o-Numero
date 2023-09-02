54b60ab9acdde482b9e55abd9102c6009701876d
Walter <walterfilho1000@gmail.com>
2023-09-02 16:07:03 -0300

    app .v1

---
+++ app/src/main/java/filho/walter/acerteonumero/IntervaloNumeros.kt
@@ -0,0 +1,68 @@
+package filho.walter.acerteonumero
+
+class IntervaloNumeros(private var min: Int, private var max: Int){
+
+    private val numeroSorteado = (min+1 until max).random()
+    private var status = JogoStatus.EXECUTANDO
+    private lateinit var numeroStatus: NumeroStatus
+
+    fun getMin(): Int {
+        return this.min
+    }
+
+    fun getMax(): Int {
+        return this.max
+    }
+
+    fun getStatus(): JogoStatus {
+        return this.status
+    }
+
+    fun getNumeroStatus(): NumeroStatus {
+        return this.numeroStatus
+    }
+
+    private fun isInvalid(numero: Int) : Boolean {
+        return numero <= this.min || numero >= this.max
+    }
+
+    private fun mudarLimites(numero: Int) {
+        if(numero < this.numeroSorteado) {
+            this.min = numero
+            this.numeroStatus = NumeroStatus.MENOR_QUE_O_SORTEADO
+        } else if(numero > this.numeroSorteado) {
+            this.max = numero
+            this.numeroStatus = NumeroStatus.MAIOR_QUE_O_SORTEADO
+        }
+        else {
+            this.numeroStatus = NumeroStatus.ACERTOU
+        }
+    }
+
+    private fun fimIntervalo(): Boolean {
+        return this.min+1 == this.max-1
+    }
+
+    public fun chute(numero: Int){
+        if(isInvalid(numero)) {
+            this.status = JogoStatus.PERDEU
+        } else if(numero == this.numeroSorteado) {
+            this.status = JogoStatus.GANHOU
+        } else {
+            mudarLimites(numero)
+            if(fimIntervalo()) {
+                this.status = JogoStatus.PERDEU
+            }
+        }
+
+    }
+
+}
+
+enum class JogoStatus {
+    GANHOU, PERDEU, EXECUTANDO
+}
+
+enum class NumeroStatus {
+    MAIOR_QUE_O_SORTEADO, MENOR_QUE_O_SORTEADO, ACERTOU
+}
---
+++ app/src/main/java/filho/walter/acerteonumero/MainActivity.kt
@@ -0,0 +1,52 @@
+package filho.walter.acerteonumero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textViewMin: TextView
+    private lateinit var textViewMax: TextView
+    private lateinit var editTextPalpite: EditText
+    private lateinit var textViewStatus: TextView
+    private lateinit var buttonChute: Button
+    private lateinit var intervaloNumeros: IntervaloNumeros
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.textViewMin = findViewById(R.id.textViewMin)
+        this.textViewMax = findViewById(R.id.textViewMax)
+        this.editTextPalpite = findViewById(R.id.editTextPalpite)
+        this.textViewStatus = findViewById(R.id.textViewStatus)
+        this.buttonChute = findViewById(R.id.buttonChute)
+        this.intervaloNumeros = IntervaloNumeros(1, 100)
+        this.updateLayoutInfo()
+
+        this.buttonChute.setOnClickListener {
+            Chute()
+        }
+    }
+
+    private fun Chute(){
+        val chute = this.editTextPalpite.text.toString()
+        if (chute != ""){
+            this.intervaloNumeros.chute(Integer.parseInt(chute))
+        }
+        this.updateLayoutInfo()
+        Toast.makeText(this, this.intervaloNumeros.getNumeroStatus().toString(),
+            Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateLayoutInfo() {
+        this.editTextPalpite.setText("")
+        this.textViewMin.text = this.intervaloNumeros.getMin().toString()
+        this.textViewMax.text = this.intervaloNumeros.getMax().toString()
+        this.textViewStatus.text = this.intervaloNumeros.getStatus().toString()
+    }
+}
